5a5236e68e4900783d12f57c069a45aed307b626
kagawagao <kingsongao1221@gmail.com>
2024-06-04 16:17:39 +0800

    chore: remove extra files

--- site/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/gaojingsong/Workspaces/github/pixas/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/gaojingsong/Workspaces/github/pixas/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/gaojingsong/Workspaces/github/pixas/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/gaojingsong/Workspaces/github/pixas/site/src/css/custom.css"),
-];
