19f8bf057ebac52e424a1f76c23b8c2140d541c6
kagawagao <kingsongao1221@gmail.com>
2024-06-06 00:59:34 +0800

    fix(cli): fix port parse from env

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -19,26 +19,8 @@ export interface DevProgramOptions extends Omit<StartOptions, 'host'> {
  */
 program
   .usage('[options]')
-  .option('-i, --ip [ip]', 'host to use', '0.0.0.0')
-  .option(
-    '-p, --port [port]',
-    'port to use',
-    (v) => {
-      const parsedV = parseInt(v);
-      if (isNaN(parsedV)) {
-        const portFromEnv = +process.env.PORT;
-
-        if (isNaN(portFromEnv)) {
-          return 3000;
-        } else {
-          return portFromEnv;
-        }
-      } else {
-        return parsedV;
-      }
-    },
-    3000,
-  )
+  .option('-i, --ip [ip]', 'host to use, default is 0.0.0.0')
+  .option('-p, --port [port]', 'port to use, default is 3000')
   .option('-b, --bundler [bundler]', 'bundler to use', app.config.bundler || 'webpack')
   .option('-e, --env [env]', 'env to use')
   .action(async () => {
